5a96830de08595571e399f2d51f57b6c24ed60f3
MingusHellstrom <unordmfhel@unord.dk>
2023-02-28 11:22:30 +0100

    lavede window

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pygame
 
 pygame.init()
+pygame.display.set_caption("Tower Climberen")
+
+
+def running():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+
+def draw_window():
+    window = pygame.display.set_mode((400, 600))
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    while running():
+        draw_window()
+        clock.tick(120)
+
+
+if __name__ == "__main__":
+    main()
